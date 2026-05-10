84483d2fe2e64597522c0184a778aaeaf8551534
yush <yus-sato@kalyte.ro>
2026-05-10 19:45:59 +0900

    update

--- generate-wireguard-creds.sh
+++ generate-wireguard-creds.sh
@@ -5,7 +5,7 @@ wg genkey |
         echo {} && echo {} | wg pubkey && wg genpsk
     ' | tr '\n' , |
     awk -F"," '{
-        print "          wireguard_private_key: \""$1"\"";
-        print "          wireguard_public_key: \""$2"\"";
-        print "          wireguard_psk_seed: \""$3"\"";
+        print "            private_key: \""$1"\"";
+        print "            public_key: \""$2"\"";
+        print "            psk_seed: \""$3"\"";
     }'
